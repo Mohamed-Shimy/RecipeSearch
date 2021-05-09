3c8065675c0d5bc245a235cb061ca53988284150
Mohamed Shemy <msh.work@outlook.sa>
2021-05-09 18:09:32 +0200

    Setup project structure

---
+++ RecipeSearch/App/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  RecipeSearch
+//
+//  Created by Mohamed Shemy on Sun 9 May 2021.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate
+{
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool
+    {
+        return true
+    }
+}
+
---
+++ RecipeSearch/Modules/Home/Controller/HomeViewController.swift
@@ -0,0 +1,14 @@
+//
+//  HomeViewController.swift
+//  RecipeSearch
+//
+//  Created by Mohamed Shemy on Sun 9 May 2021.
+//  Copyright © 2021 Mohamed Shemy. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController : UIViewController
+{
+    
+}
